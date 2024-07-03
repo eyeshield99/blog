2c2e4bd69e0932b7b8c7529b4bfbe89bcf2bf82c
Chester Jeff Ancheta <cjancheta@usm.edu.ph>
2024-07-03 15:58:59 +0800

    fix: test

--- project/settings.py
+++ project/settings.py
@@ -109,11 +109,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = "/static/"
+
 if DEBUG:
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-    STATIC_URL = "/static/"
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
